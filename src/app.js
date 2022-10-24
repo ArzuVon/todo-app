d5363f544c017f851fa5349e27bd08ff9b3f47ba
Von <107226923+ArzuVon@users.noreply.github.com>
2022-10-24 18:47:53 -0400

    app setup for todo list

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootsrap/dist/css/bootstrap.min.css';
+
+import ToDO from './components/todo/list';
+
+
+function App () {
+    return(
+        <>
+        <ToDO />
+        </>
+    )
+}

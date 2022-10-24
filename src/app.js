f1d8cd5054f1b7c9338c674cfa6d8b7909582958
Von <107226923+ArzuVon@users.noreply.github.com>
2022-10-24 19:28:39 -0400

    testing npm start index.html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import 'bootsrap/dist/css/bootstrap.min.css';
-
-import ToDO from './components/todo/list';
+import Header from './components/header/header'
+import ToDO from './components/todo/form';
+import Footer from './components/Footer/footer'
 
 
 function App () {
     return(
         <>
+        <Header />
         <ToDO />
+        <Footer />
         </>
     )
 }
+
+export default App;
